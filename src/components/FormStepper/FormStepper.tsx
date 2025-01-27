54fa10ce22ec95c6ed8bd1ea122aaf374b4fb500
Harish Soni <harish.soni@coditas.com>
2025-01-27 16:36:44 +0530

    Fixed the vercel issue

--- src/components/FormStepper/FormStepper.tsx
+++ src/components/FormStepper/FormStepper.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./FormStepper.scss";
 import BorderAnimation from "../BorderAnimation/BorderAnimation";
-import Axios from "axios";
 
 export interface IStepProps {
   placeholder: string;
